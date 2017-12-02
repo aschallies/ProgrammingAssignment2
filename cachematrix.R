728aff8736217c766ed48fd6171c618f98bafb9b
aschallies <32945151+aschallies@users.noreply.github.com>
2017-12-02 11:07:28 -0500

    Update cachematrix.R
    aschallies coding

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function computes inverse of Matrix (aschallies)
 
-## Write a short comment describing this function
+## it works
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x= matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    set <<- y
+    inv <<- NULL
 }
+get <- function() x 
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+  
 
 
-## Write a short comment describing this function
+## this funtion cache the inverse of matrix (aschallies)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
